50db863c58a57910effabe6c97c9fca84404dfda
hseleiro <hseleiro@gmail.com>
2025-06-29 16:08:34 +0100

    First code push

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+export const appConfig: { providers: any[] } = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes), provideClientHydration(withEventReplay())
+  ]
+};
